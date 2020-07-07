3b29ffb067cf6250c1244f0942fb70e6bbdf5be2
Nicholas Vitale <nicholasvitale@Nicholass-MacBook-Pro.local>
2020-07-07 13:24:12 -0700

    Updated interface for handling

--- www/app.js
+++ www/app.js
@@ -1,67 +1,67 @@
 /**
  * Object that holds application data and functions.
  */
-var app = {};
+ var app = {};
 
 /**
  * Data that is plotted on the canvas.
  */
-app.dataPoints1 = [];
-app.dataPoints2 = [];
-app.dataPoints3 = [];
+ app.dataPoints1 = [];
+ app.dataPoints2 = [];
+ app.dataPoints3 = [];
 
-var plotMolarity = 0;
-var movArrG = [];
-var movArrR = [];
-var movArrN = [];
-var movArrA = [];
-var movingAvgWindow = 10;
-movArrG.length = movingAvgWindow;
-movArrR.length = movingAvgWindow;
-movArrN.length = movingAvgWindow;
-movArrA.length = movingAvgWindow;
-var DCsumG = 0;
-var DCmeanG = 0;
-var DCsumR = 0;
-var DCmeanR = 0;
-var DCsumN = 0;
-var DCmeanN = 0;
-var DCsumA = 0;
-var DCmeanA = 0;
-var stamp = new Date();
-var time_tick = 0;
-var datapack1=[];
-var datapack2=[];
-var datapack3=[];
-var datapack4=[];
-var tickpack=[];
+ var plotMolarity = 0;
+ var movArrG = [];
+ var movArrR = [];
+ var movArrN = [];
+ var movArrA = [];
+ var movingAvgWindow = 10;
+ movArrG.length = movingAvgWindow;
+ movArrR.length = movingAvgWindow;
+ movArrN.length = movingAvgWindow;
+ movArrA.length = movingAvgWindow;
+ var DCsumG = 0;
+ var DCmeanG = 0;
+ var DCsumR = 0;
+ var DCmeanR = 0;
+ var DCsumN = 0;
+ var DCmeanN = 0;
+ var DCsumA = 0;
+ var DCmeanA = 0;
+ var stamp = new Date();
+ var time_tick = 0;
+ var datapack1=[];
+ var datapack2=[];
+ var datapack3=[];
+ var datapack4=[];
+ var tickpack=[];
 
-var isConnected = false;
+ var isConnected = false;
 
-app.CONNECT_TIMEOUT = 120000;
+ app.CONNECT_TIMEOUT = 120000;
 
 /**
  * Object that holds SensorTag UUIDs and general device information.
  */
-app.sensortag = {};
-var deviceName = 'GUTSENS';
-app.sensortag.SENSOR_SERVICE = '0000b001-1212-efde-1523-785fef13d123';
-app.sensortag.TEMP = '0000c001-1212-efde-1523-785fef13d123';
-app.sensortag.GREEN = '0000c002-1212-efde-1523-785fef13d123';
-app.sensortag.RED= '0000c003-1212-efde-1523-785fef13d123';
-app.sensortag.NIR = '0000c004-1212-efde-1523-785fef13d123';
+ app.sensortag = {};
+ var deviceName = 'GUTSENS';
+ app.sensortag.SENSOR_SERVICE = '0000b001-1212-efde-1523-785fef13d123';
+ app.sensortag.TEMP = '0000c001-1212-efde-1523-785fef13d123';
+ app.sensortag.GREEN = '0000c002-1212-efde-1523-785fef13d123';
+ app.sensortag.RED= '0000c003-1212-efde-1523-785fef13d123';
+ app.sensortag.NIR = '0000c004-1212-efde-1523-785fef13d123';
 
-evothings.loadScript('libs/smoothie.js');
+ evothings.loadScript('libs/smoothie.js');
 /**
  * Initialise the application.
  */
 
-app.initialize = function()
-{
-	document.addEventListener(
-		'deviceready',
-		function() { evothings.scriptsLoaded(app.onDeviceReady) },
-		false);
+ app.initialize = function()
+ {
+ 	document.addEventListener(
+ 		'deviceready',
+ 		function() { evothings.scriptsLoaded(app.onDeviceReady) },
+ 		false);
 
 	// Called when HTML page has been loaded.
 	$(document).ready( function()
@@ -88,13 +88,13 @@ app.respondCanvas = function()
 
 //Updates the averavge values for DC tracking
 app.avgupdate= function(newValue, inArray, sum, mean)
-	{
-		var slice = inArray.slice(0,movingAvgWindow);
-		var temp_array = [newValue];
-		inArray = temp_array.concat(slice);
-		sum = inArray.reduce(function(a, b){return a + b;}, 0);
-		mean = sum/movingAvgWindow;
-	};
+{
+	var slice = inArray.slice(0,movingAvgWindow);
+	var temp_array = [newValue];
+	inArray = temp_array.concat(slice);
+	sum = inArray.reduce(function(a, b){return a + b;}, 0);
+	mean = sum/movingAvgWindow;
+};
 
 app.onDeviceReady = function()
 {
@@ -126,14 +126,14 @@ app.onStopButton = function()
 
 
 document.addEventListener("deviceready", onDeviceReady, false);
-	
-	function onDeviceReady() 
-	{
 
-		document.getElementById("writeFile").addEventListener("click", writeFile);
+function onDeviceReady() 
+{
+
+	document.getElementById("writeFile").addEventListener("click", writeFile);
 
 
-	};
+};
 
 app.startConnectTimer = function()
 {
@@ -177,9 +177,9 @@ app.deviceIsSensorTag = function(device)
 {
 	console.log('device name: ' + device.name);
 	return (device != null) &&
-		(device.name != null) &&
-		(device.name.indexOf(deviceName) > -1 ||
-			device.name.indexOf(deviceName) > -1);
+	(device.name != null) &&
+	(device.name.indexOf(deviceName) > -1 ||
+		device.name.indexOf(deviceName) > -1);
 };
 
 app.ProgRead = function()
@@ -190,86 +190,96 @@ app.ProgRead = function()
 /**
  * Read services for a device.
  */
-app.connectToDevice = function(device)
-{
-	app.showInfo('Connecting...');
-	device.connect(
-		function(device)
-		{
-			app.showInfo('Status: wearS connected ...');
-			isConnected = true;
-			app.readServices(device);
-		},
-		function(errorCode)
-		{
-			app.showInfo('Error: connection failed: ' + errorCode + '.');
-			isConnected = false;
-			app.stopConnectTimer();
-			evothings.easyble.stopScan();
-			evothings.easyble.closeConnectedDevices();
-			evothings.ble.reset();
-		});
-};
+ app.connectToDevice = function(device)
+ {
+ 	app.showInfo('Connecting...');
+ 	device.connect(
+ 		function(device)
+ 		{
+ 			app.showInfo('Status: wearS connected ...');
+ 			isConnected = true;
+ 			app.readServices(device);
+ 		},
+ 		function(errorCode)
+ 		{
+ 			app.showInfo('Error: connection failed: ' + errorCode + '.');
+ 			isConnected = false;
+ 			app.stopConnectTimer();
+ 			evothings.easyble.stopScan();
+ 			evothings.easyble.closeConnectedDevices();
+ 			evothings.ble.reset();
+ 		});
+ };
 
-app.readServices = function(device)
-{
-	
-	if(program_enabled)
-	{
-        device.readServices(
-		[app.sensortag.SENSOR_SERVICE],
-		function(){hyper.log('Services Read')
-		var arra = new Uint8Array(1);
-		arra[0] = prog_pack[0];
-	device.writeCharacteristic(
-		      "0000c003-1212-efde-1523-785fef13d123",
+ app.readServices = function(device)
+ {
+
+ 	if(program_enabled)
+ 	{
+ 		device.readServices(
+ 			[app.sensortag.SENSOR_SERVICE],
+ 			function(){hyper.log('Services Read')
+ 			var arra = new Uint8Array(10);
+ 			arra[0] = prog_pack[0];
+ 			arra[1] = prog_pack[1];
+ 			arra[2] = prog_pack[2];
+ 			arra[3] = prog_pack[3];
+ 			arra[4] = prog_pack[4];
+ 			arra[5] = prog_pack[5];
+ 			arra[6] = prog_pack[6];
+ 			arra[7] = prog_pack[7];
+ 			arra[8] = prog_pack[8];
+ 			arra[9] = prog_pack[9];
+ 			
+ 			device.writeCharacteristic(
+ 				"0000c003-1212-efde-1523-785fef13d123",
 		      arra, // Write byte with value og programming pack.
 		      function()
 		      {
-		        console.log('BLE characteristic written.');
+		      	console.log('BLE characteristic written.');
 		      },
 		      function(errorCode)
 		      {
-		        console.log('BLE writeDescriptor error: ' + errorCode);
+		      	console.log('BLE writeDescriptor error: ' + errorCode);
 		      });},
 		// Use this function to monitor magnetometer data
 		function(errorCode)
 		{
 			console.log('Error: failed to read services: ' + errorCode + '.');
 		});
-    program_enabled = 0;    
-        
+ 		program_enabled = 0;    
 
 
 
-	}
-	else
-	{
-device.readServices(
-		[app.sensortag.SENSOR_SERVICE],
-		app.startNotification,
+
+ 	}
+ 	else
+ 	{
+ 		device.readServices(
+ 			[app.sensortag.SENSOR_SERVICE],
+ 			app.startNotification,
 		// Use this function to monitor magnetometer data
 		function(errorCode)
 		{
 			console.log('Error: failed to read services: ' + errorCode + '.');
 		});
 
-	}
+ 	}
 
-};
+ };
 
-app.startNotification = function(device)
-{
-	app.showInfo('Status: Data Streaming ...');
+ app.startNotification = function(device)
+ {
+ 	app.showInfo('Status: Data Streaming ...');
 
-	device.enableNotification(
-		app.sensortag.GREEN,
-		function(data)
-		{
-			hyper.log(evothings.util.typedArrayToHexString(data))
-			var dataArray = new Uint8Array(data);
-			var canvas = document.getElementById('canvas2');
-			var context = canvas.getContext('2d');
+ 	device.enableNotification(
+ 		app.sensortag.GREEN,
+ 		function(data)
+ 		{
+ 			hyper.log(evothings.util.typedArrayToHexString(data))
+ 			var dataArray = new Uint8Array(data);
+ 			var canvas = document.getElementById('canvas2');
+ 			var context = canvas.getContext('2d');
 			//Now we need to split the 8byte array into two 4byte chonks 
 			var d1 = dataArray.slice(0,4); //Grabs Red reg
 			var d2 = dataArray.slice(4,8); //Grabs Green reg
@@ -283,7 +293,6 @@ app.startNotification = function(device)
 			var dataSensor4 = evothings.util.littleEndianToUint32(d4,0)*(1.2)/(Math.pow(2,21))
 
 			var tick = evothings.util.littleEndianToUint32(time_stamp,0);
-
 
 			datapack1.push(dataSensor1);
 			datapack2.push(dataSensor2);
@@ -303,10 +312,10 @@ app.startNotification = function(device)
 			line3.append(new Date().getTime(), dataSensor3);
 			line4.append(new Date().getTime(), dataSensor4);
 
-		    line1dc.append(new Date().getTime(), DCmeanR);
-		    line2dc.append(new Date().getTime(), DCmeanG);
-		    line3dc.append(new Date().getTime(), DCmeanN);
-		    line4dc.append(new Date().getTime(), DCmeanA);
+			line1dc.append(new Date().getTime(), DCmeanR);
+			line2dc.append(new Date().getTime(), DCmeanG);
+			line3dc.append(new Date().getTime(), DCmeanN);
+			line4dc.append(new Date().getTime(), DCmeanA);
 
 			document.getElementById('info1').innerHTML = 'Red CH.:'+ dataSensor1 + 'V' ;
 			document.getElementById('info2').innerHTML = 'Green CH.:'+ dataSensor2 + 'V';
@@ -319,68 +328,65 @@ app.startNotification = function(device)
 			console.log('Error: enableNotification: ' + errorCode + '.');
 		});
 
-};
+ };
 
-stamp = new Date();
-function createFile() {
-	 var type = window.TEMPORARY;
-	 var size = 5*1024*1024;
-	 logfilename = "WearS" + "_" + stamp.getFullYear() + "_" + stamp.getMonth() + "_" + stamp.getDate() + "_" + stamp.getHours() + "_" + stamp.getMinutes() + "_" + stamp.getSeconds() + ".txt";
-	 window.requestFileSystem(type, size, successCallback, errorCallback)
+ stamp = new Date();
+ function createFile() {
+ 	var type = window.TEMPORARY;
+ 	var size = 5*1024*1024;
+ 	logfilename = "WearS" + "_" + stamp.getFullYear() + "_" + stamp.getMonth() + "_" + stamp.getDate() + "_" + stamp.getHours() + "_" + stamp.getMinutes() + "_" + stamp.getSeconds() + ".txt";
+ 	window.requestFileSystem(type, size, successCallback, errorCallback)
 
-	 function successCallback(fs) {
-			fs.root.getFile(logfilename, {create: true, exclusive: true}, function(fileEntry) {
-				 alert('File creation successfull!')
-			}, errorCallback);
-	 }
+ 	function successCallback(fs) {
+ 		fs.root.getFile(logfilename, {create: true, exclusive: true}, function(fileEntry) {
+ 			alert('File creation successfull!')
+ 		}, errorCallback);
+ 	}
 
-	 function errorCallback(error) {
-			alert("ERROR: " + error.code)
-	 }
+ 	function errorCallback(error) {
+ 		alert("ERROR: " + error.code)
+ 	}
 
-}
+ }
 
 //
 
 
 
 function writeFile() {
-   var type = window.TEMPORARY;
-   var size = 5*1024*1024;
-	 var stamp = new Date();
+	var type = window.TEMPORARY;
+	var size = 5*1024*1024;
+	var stamp = new Date();
 
-   logfilename = "wearS" + "_" + stamp.getFullYear() + "_" + (stamp.getMonth()+1) + "_" + stamp.getDate() + "_" + stamp.getHours() + "_" + stamp.getMinutes() + "_" + stamp.getSeconds() + ".txt";
-   window.requestFileSystem(type, size, successCallback, errorCallback)
+	logfilename = "wearS" + "_" + stamp.getFullYear() + "_" + (stamp.getMonth()+1) + "_" + stamp.getDate() + "_" + stamp.getHours() + "_" + stamp.getMinutes() + "_" + stamp.getSeconds() + ".txt";
+	window.requestFileSystem(type, size, successCallback, errorCallback)
 
-   function successCallback(fs) {
+	function successCallback(fs) {
 
-      fs.root.getFile(logfilename, {create: true, exclusive: true}, function(fileEntry) {
+		fs.root.getFile(logfilename, {create: true, exclusive: true}, function(fileEntry) {
 
-         fileEntry.createWriter(function(fileWriter) {
-            fileWriter.onwriteend = function(e) {
-               alert('Write completed.');
-            };
+			fileEntry.createWriter(function(fileWriter) {
+				fileWriter.onwriteend = function(e) {
+					alert('Write completed.');
+				};
 
-            fileWriter.onerror = function(e) {
-               alert('Write failed: ' + e.toString());
-            };
+				fileWriter.onerror = function(e) {
+					alert('Write failed: ' + e.toString());
+				};
 
             //var blob = new Blob([line1s.data + "\n" + line2s.data + "\n" + line3s.data + "\n" + line4s.data], {type: 'text/plain'});
             var blob = new Blob([datapack1 + "\n" + " END OF RED DATA " + "\n" + datapack2 + "\n" +" END OF GREEN DATA " + "\n" + datapack3 + "\n" +" END OF NIR DATA " + "\n" + datapack4 + "\n" +" END OF DATA " + "\n" + tickpack], {type: 'text/plain'});
             fileWriter.write(blob);
-         }, errorCallback);
+        }, errorCallback);
 
-      }, errorCallback);
+		}, errorCallback);
 
-   }
+	}
 
-   function errorCallback(error) {
-      alert("ERROR: " + error.code)
-   }
+	function errorCallback(error) {
+		alert("ERROR: " + error.code)
+	}
 
 }
-
-//
-
 
 app.initialize();
--- www/prog.js
+++ www/prog.js
@@ -1,7 +1,13 @@
+//Support JS for gathering up all of the programming options 
+
 var prog = {};
 var developer_mode = false;
+var sep_gain_mode = false;
 var prog_pack = [];
+var LED_Control = [];
 var program_enabled = false;
+var tia_res_char = ['500k','250k','100k','50k', '25k','10k','1M','2M'];
+var tia_cap_char = ['5p','2.5p','10p','7.5p', '20p','17.5p','25p','22.5p'];
 
 
 //Bit masks for LED phase modes 
@@ -12,7 +18,16 @@ var red_mask = 0x04
 var rn_mask = 0x05
 var rg_mask = 0x06
 var rgn_mask = 0x07
-var phase_mode = 0x07;
+var phase_mode = 0x07; //set by default
+var LED1_drive = 0x03;//set by default
+var LED2_drive = 0x03;//set by default
+var LED3_drive = 0x03;//set by default
+var TIA_R1 = 0x02;
+var TIA_C1 = 0x00;
+var TIA_R2 = 0x02;
+var TIA_C2 = 0x00;
+
+
 
 evothings.loadScript('libs/evothings/easyble/easyble.js');
 
@@ -49,6 +64,74 @@ prog.onDeviceReady = function()
 {
 };
 
+prog.stepperInput = function(id, s, m) 
+{
+
+	var el = document.getElementById(id);
+	if (s > 0) {
+		if (parseInt(el.value) < m) {
+			el.value = parseInt(el.value) + s;
+		}
+	} else {
+		if (parseInt(el.value) > m) {
+			el.value = parseInt(el.value) + s;
+		}
+	}
+	switch(el.id)
+	{
+		case "stepperLED1":
+
+		var current = Number(((el.value)*1.6).toPrecision(3));
+
+		document.getElementById('LED1_current').textContent = 'L1 Set ->' + current + 'mA';
+
+	  	LED1_drive = el.value; //save the current settings
+
+	  	break;
+
+	  	case "stepperLED2":
+
+	  	var current = Number(((el.value)*1.6).toPrecision(3));
+
+	  	document.getElementById('LED2_current').textContent = 'L2 Set ->' + current + 'mA';
+	  	LED2_drive = el.value; //save the current settings
+
+	  	break;
+
+	  	case "stepperLED3":
+
+	  	var current = Number(((el.value)*1.6).toPrecision(3));
+
+	  	document.getElementById('LED3_current').textContent = 'L3 Set ->' + current + 'mA';
+	  	LED3_drive = el.value; //save the current settings
+
+	  	break;
+
+	  	case "tiar1":
+  		TIA_R1 = el.value; //save the current settings
+  		document.getElementById('tia_gain_r1').textContent = 'R1 ->' + tia_res_char[TIA_R1];
+  		break;
+
+  		case "tiac1":
+  		TIA_C1 = el.value; //save the current settings
+  		document.getElementById('tia_gain_c1').textContent = 'C1 ->' + tia_cap_char[TIA_C1];
+  		break;
+
+  		case "tiar2":
+  		TIA_R2 = el.value; //save the current settings
+  		document.getElementById('tia_gain_r2').textContent = 'R2 ->' + tia_res_char[TIA_R2];
+  		break;
+
+  		case "tiac2":
+  		TIA_C2 = el.value; //save the current settings
+  		document.getElementById('tia_gain_c2').textContent = 'C2 ->' + tia_cap_char[TIA_C2];
+  		break;
+
+  		default:
+  		break;
+  	}
+  }
+
 //Function to change the LED selection
 prog.onLEDSelect = function(clicked_id)
 {
@@ -57,84 +140,84 @@ prog.onLEDSelect = function(clicked_id)
 	switch(clicked_id)
 	{
 		case "L1":
-			document.getElementById(clicked_id).className  = "green"
-			document.getElementById("L2").className  = "gray"
-			document.getElementById("L3").className  = "gray"
-			document.getElementById("L4").className  = "gray"
-			document.getElementById("L5").className  = "gray"
-			document.getElementById("L6").className  = "gray"
-			document.getElementById("L7").className  = "gray"
-			phase_mode = rgn_mask;
+		document.getElementById(clicked_id).className  = "green"
+		document.getElementById("L2").className  = "gray"
+		document.getElementById("L3").className  = "gray"
+		document.getElementById("L4").className  = "gray"
+		document.getElementById("L5").className  = "gray"
+		document.getElementById("L6").className  = "gray"
+		document.getElementById("L7").className  = "gray"
+		phase_mode = rgn_mask;
 		break;
 
 		case "L2":
-		    document.getElementById(clicked_id).className  = "green"
-			document.getElementById("L1").className  = "gray"
-			document.getElementById("L3").className  = "gray"
-			document.getElementById("L4").className  = "gray"
-			document.getElementById("L5").className  = "gray"
-			document.getElementById("L6").className  = "gray"
-			document.getElementById("L7").className  = "gray"
-			phase_mode = rg_mask;
+		document.getElementById(clicked_id).className  = "green"
+		document.getElementById("L1").className  = "gray"
+		document.getElementById("L3").className  = "gray"
+		document.getElementById("L4").className  = "gray"
+		document.getElementById("L5").className  = "gray"
+		document.getElementById("L6").className  = "gray"
+		document.getElementById("L7").className  = "gray"
+		phase_mode = rg_mask;
 
 		break;
 
 		case "L3":
-		    document.getElementById(clicked_id).className  = "green"
-			document.getElementById("L1").className  = "gray"
-			document.getElementById("L2").className  = "gray"
-			document.getElementById("L4").className  = "gray"
-			document.getElementById("L5").className  = "gray"
-			document.getElementById("L6").className  = "gray"
-			document.getElementById("L7").className  = "gray"
-			phase_mode = gn_mask;
+		document.getElementById(clicked_id).className  = "green"
+		document.getElementById("L1").className  = "gray"
+		document.getElementById("L2").className  = "gray"
+		document.getElementById("L4").className  = "gray"
+		document.getElementById("L5").className  = "gray"
+		document.getElementById("L6").className  = "gray"
+		document.getElementById("L7").className  = "gray"
+		phase_mode = gn_mask;
 		break;
 
 		case "L4":
-			document.getElementById(clicked_id).className  = "green"
-			document.getElementById("L1").className  = "gray"
-			document.getElementById("L2").className  = "gray"
-			document.getElementById("L3").className  = "gray"
-			document.getElementById("L5").className  = "gray"
-			document.getElementById("L6").className  = "gray"
-			document.getElementById("L7").className  = "gray"
-			phase_mode = rn_mask;
+		document.getElementById(clicked_id).className  = "green"
+		document.getElementById("L1").className  = "gray"
+		document.getElementById("L2").className  = "gray"
+		document.getElementById("L3").className  = "gray"
+		document.getElementById("L5").className  = "gray"
+		document.getElementById("L6").className  = "gray"
+		document.getElementById("L7").className  = "gray"
+		phase_mode = rn_mask;
 
 		break;
 
 		case "L5":
-			document.getElementById(clicked_id).className  = "green"
-			document.getElementById("L1").className  = "gray"
-			document.getElementById("L2").className  = "gray"
-			document.getElementById("L3").className  = "gray"
-			document.getElementById("L4").className  = "gray"
-			document.getElementById("L6").className  = "gray"
-			document.getElementById("L7").className  = "gray"
-			phase_mode = red_mask;
+		document.getElementById(clicked_id).className  = "green"
+		document.getElementById("L1").className  = "gray"
+		document.getElementById("L2").className  = "gray"
+		document.getElementById("L3").className  = "gray"
+		document.getElementById("L4").className  = "gray"
+		document.getElementById("L6").className  = "gray"
+		document.getElementById("L7").className  = "gray"
+		phase_mode = red_mask;
 
 		break;
 
 		case "L6":
-			document.getElementById(clicked_id).className  = "green"
-			document.getElementById("L1").className  = "gray"
-			document.getElementById("L2").className  = "gray"
-			document.getElementById("L3").className  = "gray"
-			document.getElementById("L4").className  = "gray"
-			document.getElementById("L5").className  = "gray"
-			document.getElementById("L7").className  = "gray"
-			phase_mode = green_mask;
+		document.getElementById(clicked_id).className  = "green"
+		document.getElementById("L1").className  = "gray"
+		document.getElementById("L2").className  = "gray"
+		document.getElementById("L3").className  = "gray"
+		document.getElementById("L4").className  = "gray"
+		document.getElementById("L5").className  = "gray"
+		document.getElementById("L7").className  = "gray"
+		phase_mode = green_mask;
 
 		break;
 
 		case "L7":
-			document.getElementById(clicked_id).className  = "green"
-			document.getElementById("L2").className  = "gray"
-			document.getElementById("L3").className  = "gray"
-			document.getElementById("L4").className  = "gray"
-			document.getElementById("L5").className  = "gray"
-			document.getElementById("L6").className  = "gray"
-			document.getElementById("L1").className  = "gray"
-			phase_mode = nir_mask;
+		document.getElementById(clicked_id).className  = "green"
+		document.getElementById("L2").className  = "gray"
+		document.getElementById("L3").className  = "gray"
+		document.getElementById("L4").className  = "gray"
+		document.getElementById("L5").className  = "gray"
+		document.getElementById("L6").className  = "gray"
+		document.getElementById("L1").className  = "gray"
+		phase_mode = nir_mask;
 
 		break;
 
@@ -143,7 +226,7 @@ prog.onLEDSelect = function(clicked_id)
 
 	}
 
-	}
+}
 
 //Check the developer mode. If its OFF, then we are only using the calibration mode and on the phase type can be modified (ie. green on, red on, nir on, single and dual phase items)
 //On the ble device, the AFE will be program accordingly. If the the device is programmed to dual phase, one of the leds will be used as an ambient phase for individual ambient cancellation in the calibration code. 
@@ -151,32 +234,85 @@ prog.developerCheck = function(toggle_id)
 {
 	if(document.getElementById(toggle_id).checked)
 	{
-		document.getElementById("developer_mode_text").innerHTML = 'Developer Mode ON'
+		document.getElementById("developer_mode_text").innerHTML = 'Developer Mode : ON'
+		document.getElementById("developer_mode_text").style.fontWeight = "bold"
 		developer_mode = true;
 	}
 	else
 	{
-		document.getElementById("developer_mode_text").innerHTML = 'Developer Mode OFF'
+		document.getElementById("developer_mode_text").innerHTML = 'Developer Mode : OFF'
+		document.getElementById("developer_mode_text").style.fontWeight = "bold"
 		developer_mode = false;
 	}
+}
+
+//Check if the user wants to enable separate gain for the  red/nir phases
+prog.sepGainCheck = function(toggle_id)
+{
+	if(document.getElementById(toggle_id).checked)
+	{
+		document.getElementById("sep_gain_mode_text").innerHTML = 'Enable Sep Gain : ON'
+		document.getElementById("sep_gain_mode_text").style.fontWeight = "bold"
+		sep_gain_mode = true;
+	}
+	else
+	{
+		document.getElementById("sep_gain_mode_text").innerHTML = 'Enable Sep Gain : OFF'
+		document.getElementById("sep_gain_mode_text").style.fontWeight = "bold"
+		sep_gain_mode = false;
+	}
+}
+
+prog.gain_pack =function()
+{
+//If sep gain mode is enabled, we need to gather its contents for writing to TIAGAIN sector
+			if(sep_gain_mode)
+			{
+				prog_pack[5] = 0x01; //Represents the separate gain 
+				prog_pack[6] = TIA_R1;
+				prog_pack[7] = TIA_C1;
+				prog_pack[8] = TIA_R2;
+				prog_pack[9] = TIA_C2;
+			}
+
+			else
+			{
+				prog_pack[5] = 0x00; //Represents the separate gain 
+				prog_pack[6] = TIA_R1;
+				prog_pack[7] = TIA_C1;
+				prog_pack[8] = 0x00; //Get rid of the secondary options
+				prog_pack[9] = 0x00;
+			}
+
 }
 
 //User wants to gather all the options together 
 prog.program = function()
 {
-	if(!(this.isConnected))
+	if(!isConnected)
 	{
 		//If the developer mode is disabled, we are only allowed to send the phase scheme over to the BLE side of things. If the developer mode is enabled, then we need to send all of the configurations over.  
-		if(!(this.developer_mode))
+		if(!developer_mode)
 		{
 			program_enabled =true; //Use a flag to let app.js know we only want to program the device and NOT stream data
-			prog_pack[0] = phase_mode;
+			prog_pack[0] = 0x00; //Developer mode disabled
+			prog_pack[1] = phase_mode; //Pack the phase mode in and then let the app do the calibration
 			document.getElementById('start_button').click() //We want to tell app.js that we want to connect and program the device 
 		}
 
+		else if(developer_mode) //if the developer mode is enabled then it allows the user to send the all other items other than led phase selection
+		{
+			program_enabled =true; //Use a flag to let app.js know we only want to program the device and NOT stream data
+			prog_pack[0] = 0x01; //Developer mode enabled
+			prog_pack[1] = phase_mode; //Pack the phase mode in
+			prog_pack[2] = LED1_drive;
+			prog_pack[3] = LED2_drive;
+			prog_pack[4] = LED3_drive;
 
-
-
+			prog.gain_pack();
+			document.getElementById('start_button').click()
+			
+		}
 	}
 	else
 	{
